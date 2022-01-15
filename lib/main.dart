a024107b6ad5844d6ff92ae674f0919186b296cc
klsh1tt <klsh1tt@gmail.com>
2022-01-15 21:51:05 +0900

    カバーページの実装とfirebaseとfirestoreの記述の削除

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:five_senses_tell_draft/presentations/cover/cover_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -7,17 +7,14 @@ import 'package:intl/date_symbol_data_local.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDateFormatting('ja_JP');
-  await Firebase.initializeApp();
-  SystemChrome.setPreferredOrientations(
-          [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight])
-      .then((_) => {
-            runApp(
-              //Riverpodを使うための設定
-              ProviderScope(
-                child: FiveSensesTellDraft(),
-              ),
-            )
-          });
+  SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]).then((_) => {
+        runApp(
+          //Riverpodを使うための設定
+          ProviderScope(
+            child: FiveSensesTellDraft(),
+          ),
+        )
+      });
 }
 
 class FiveSensesTellDraft extends StatelessWidget {
@@ -28,6 +25,7 @@ class FiveSensesTellDraft extends StatelessWidget {
     return MaterialApp(
       home: CoverPage(),
       title: 'FiveSensesTellDraft',
+      theme: ThemeData.light(),
     );
   }
 }
---
+++ lib/presentations/parts/loading_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class LoadingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child:Image.asset('assets/images/loading.gif'));
+  }
+}
