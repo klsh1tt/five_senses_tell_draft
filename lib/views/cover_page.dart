5ef2a668c2d27466f35803ded831e184d3ad88b8
klsh1tt <klsh1tt@gmail.com>
2022-01-23 16:59:15 +0900

    WIP タブページの実装

--- lib/views/cover_page.dart
+++ lib/views/cover_page.dart
@@ -1,25 +1,52 @@
 import 'dart:io';
 
+import 'package:five_senses_tell_draft/views/second_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'first_page.dart';
+
+final pageTypeProvider = StateProvider<PageType>((ref) => PageType.first);
+
+enum PageType {
+  first,
+  second,
+}
 
 class CoverPage extends StatelessWidget {
-  const CoverPage({Key? key}) : super(key: key);
+  final List<Widget> _pageList = <Widget>[
+    FirstPage(),
+    SecondPage(),
+  ];
+  
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('FiveSensesTellDraft'),
-        ),
-        body: Center(
-          child: Text(
-            'ここはカバーページです',
-            style: TextStyle(
-              fontSize: 60,
-            ),
+    return Consumer(
+      builder: (context, watch, child) {
+        final pageType = ;
+
+        final tabItems = [
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.face),
+            label: '',
           ),
-        ),
-      ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.fastfood),
+            label: '',
+          ),
+        ];
+
+        return Scaffold(
+          body: _pageList[pageType.state.index],
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: pageType.state.index,
+            onTap: (index) {
+              pageType.state = PageType.values[index];
+            },
+            items: tabItems,
+          ),
+        );
+      },
     );
   }
 }
